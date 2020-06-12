5ecc0590bf0a385ff7961faa3b96e4446730e192
Ben Langmead <langmea@cs.jhu.edu>
2020-06-12 06:30:24 -0400

    improvements to mouse mkreads

--- amazonlinux/mk_mousereads/reads100.sh
+++ amazonlinux/mk_mousereads/reads100.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+set -ex
+
 SEED=765
 
 cd /work
 
-python reads.py --prefix=mix100 \
+chmod a+x reads.py
+./reads.py --prefix=mix100 \
 	--temp-dir=mix100_temp \
 	--reads-per-accession 2000000 \
 	--seed ${SEED}
