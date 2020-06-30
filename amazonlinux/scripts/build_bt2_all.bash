9efd7fc14665659483b769d5017aba3ca23065d6
Ben Langmead <langmea@cs.jhu.edu>
2020-06-30 13:48:37 -0400

    use bug_fixes branch

--- amazonlinux/scripts/build_bt2_all.bash
+++ amazonlinux/scripts/build_bt2_all.bash
@@ -2,18 +2,24 @@
 
 set -ex
 
+BRANCH="${1}"
+
+if [[ -z "${BRANCH}" ]] ; then
+    BRANCH=master
+fi
+
 test -n "${THREADS}"
 
 if [ "${ARCH}" = "aarch64" ] ; then
     git clone --recursive https://github.com/BenLangmead/bowtie2.git /work/bowtie2 \
         && cd /work/bowtie2 \
-        && git checkout v2.4.1 \
+        && git checkout "${BRANCH}" \
         && sed -i.bak 's/CXXFLAGS :=/CXXFLAGS +=/' Makefile \
         && sed -i.bak 's/CPPFLAGS :=/CPPFLAGS +=/' Makefile \
         && make POPCNT_CAPABILITY=0 -j "${THREADS}" allall
 else
     git clone --recursive https://github.com/BenLangmead/bowtie2.git /work/bowtie2 \
         && cd /work/bowtie2 \
-        && git checkout v2.4.1 \
+        && git checkout "${BRANCH}" \
         && make -j "${THREADS}" allall
 fi
