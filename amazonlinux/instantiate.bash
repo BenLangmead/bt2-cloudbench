ce59500488ddb7648c2bc54b782436454edc3690
Ben Langmead <langmea@cs.jhu.edu>
2020-06-30 13:27:10 -0400

    include instance type in working directory name

--- amazonlinux/instantiate.bash
+++ amazonlinux/instantiate.bash
@@ -5,4 +5,4 @@ prof=$(grep '"default_profile":' profile.json | sed 's/"//g' | awk '{print $2}')
 test -z "${prof}" && echo "Could not parse default profile from profile.json" && exit 1
 
 rm -rf ".${1}" \
-  && ./vagrant_run.py "${prof}" "${1}" "${2}" ".${1}"
+  && ./vagrant_run.py "${prof}" "${1}" "${2}" ".${1}.${2}"
