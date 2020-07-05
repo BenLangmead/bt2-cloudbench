470af9d951fe3b7a6b79743b08ea6bdc0657315b
Ben Langmead <langmea@cs.jhu.edu>
2020-07-05 14:23:09 -0400

    add bt2 build script

---
+++ amazonlinux/scripts/build_bt2_build.bash
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -ex
+
+BRANCH="${1}"
+
+if [[ -z "${BRANCH}" ]] ; then
+    BRANCH=master
+fi
+
+test -n "${THREADS}"
+
+if [ "${ARCH}" = "aarch64" ] ; then
+    git clone --recursive https://github.com/BenLangmead/bowtie2.git /work/bowtie2 \
+        && cd /work/bowtie2 \
+        && git checkout "${BRANCH}" \
+        && sed -i.bak 's/CXXFLAGS :=/CXXFLAGS +=/' Makefile \
+        && sed -i.bak 's/CPPFLAGS :=/CPPFLAGS +=/' Makefile \
+        && make POPCNT_CAPABILITY=0 -j "${THREADS}" bowtie2-build-s bowtie2-build-l bowtie2-build-s-debug bowtie2-build-l-debug
+else
+    git clone --recursive https://github.com/BenLangmead/bowtie2.git /work/bowtie2 \
+        && cd /work/bowtie2 \
+        && git checkout "${BRANCH}" \
+        && make -j "${THREADS}" bowtie2-build-s bowtie2-build-l bowtie2-build-s-debug bowtie2-build-l-debug
+fi
