32531a4dc056fcf1ceb72cdc068199aed829ac52
Ben Langmead <langmea@cs.jhu.edu>
2020-07-09 20:14:11 -0400

    quieter fetching of reads

--- amazonlinux/scripts/get_reads.bash
+++ amazonlinux/scripts/get_reads.bash
@@ -7,5 +7,7 @@ SPECIES="${1}"
 mkdir -p /work/reads
 
 for i in 1 2 ; do
-    aws s3 cp "s3://bt2-bench/reads/${SPECIES}/mix100_${i}.fq" "/work/reads/mix100_${i}.fq"
+    aws s3 cp --quiet "s3://bt2-bench/reads/${SPECIES}/mix100_${i}.fq" "/work/reads/mix100_${i}.fq"
 done
+
+ls /work/reads
