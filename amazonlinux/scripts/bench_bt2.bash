0806f0973f6a6d1c39c3301e2201f39cfd15873f
Ben Langmead <langmea@cs.jhu.edu>
2020-07-09 20:20:44 -0400

    fix dir typo

--- amazonlinux/scripts/bench_bt2.bash
+++ amazonlinux/scripts/bench_bt2.bash
@@ -6,10 +6,14 @@ mkdir /work/sam
 
 [[ -z "${THREADS}" ]] && echo "THREADS must be set" && exit 1
 
+test -d /work/indexes
+test -f /work/indexes/idx.1.bt2
+test -f /work/reads/mix100_1.fq
+test -f /work/reads/mix100_2.fq
+
 /work/bowtie2/bowtie2-align-s \
-  -x /work/index/idx \
+  -x /work/indexes/idx \
   -p "${THREADS}" \
   -t \
   -1 /work/reads/mix100_1.fq -2 /work/reads/mix100_2.fq \
   -S /work/sam/out.sam
-
