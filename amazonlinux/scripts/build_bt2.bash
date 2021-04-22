146ac9f736c2860baabdd917d4bd3125ea0c6db4
Ben Langmead <ben.langmead@gmail.com>
2021-04-22 12:18:40 -0400

    update submodule handling

--- amazonlinux/scripts/build_bt2.bash
+++ amazonlinux/scripts/build_bt2.bash
@@ -2,16 +2,21 @@
 
 set -ex
 
+VER=v2.4.2
+
 if [ "${ARCH}" = "aarch64" ] ; then
     git clone --recursive https://github.com/BenLangmead/bowtie2.git /work/bowtie2 \
         && cd /work/bowtie2 \
-        && git checkout v2.4.1 \
+        && git checkout ${VER} \
+        && git submodule init \
+        && git submodule sync \
+        && git submodule update \
         && sed -i.bak 's/CXXFLAGS :=/CXXFLAGS +=/' Makefile \
         && sed -i.bak 's/CPPFLAGS :=/CPPFLAGS +=/' Makefile \
-        && make POPCNT_CAPABILITY=0 bowtie2-align-s bowtie2-build-s
+        && make -j2 POPCNT_CAPABILITY=0 bowtie2-align-s bowtie2-build-s
 else
     git clone --recursive https://github.com/BenLangmead/bowtie2.git /work/bowtie2 \
         && cd /work/bowtie2 \
-        && git checkout v2.4.1 \
-        && make bowtie2-align-s bowtie2-build-s
+        && git checkout ${VER} \
+        && make -j2 bowtie2-align-s bowtie2-build-s
 fi
