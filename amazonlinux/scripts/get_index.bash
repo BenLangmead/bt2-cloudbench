0f9ef9e293c5a7f5db295bd9e3baaae147895807
Ben Langmead <ben.langmead@gmail.com>
2021-04-22 08:54:50 -0400

    update locations of reads and indexes

--- amazonlinux/scripts/get_index.bash
+++ amazonlinux/scripts/get_index.bash
@@ -7,7 +7,7 @@ INDEX="${1}"
 mkdir -p /work/indexes
 
 for i in 1 2 3 4 rev.1 rev.2 ; do
-    aws s3 cp --quiet "s3://bt2-bench/indexes/${INDEX}.${i}.bt2" "/work/indexes/idx.${i}.bt2"
+    aws s3 cp --quiet "s3://genome-idx/bt/${INDEX}.${i}.bt2" "/work/indexes/idx.${i}.bt2"
 done
 
 ls /work/indexes
--- amazonlinux/scripts/get_reads.bash
+++ amazonlinux/scripts/get_reads.bash
@@ -3,11 +3,12 @@
 set -ex
 
 SPECIES="${1}"
+READLEN=100
 
 mkdir -p /work/reads
 
 for i in 1 2 ; do
-    aws s3 cp --quiet "s3://bt2-bench/reads/${SPECIES}/mix100_${i}.fq" "/work/reads/mix100_${i}.fq"
+    aws s3 cp --quiet "s3://genome-idx/bt/reads/${SPECIES}/mix${READLEN}_${i}.fq" "/work/reads/mix${READLEN}_${i}.fq"
 done
 
 ls /work/reads
