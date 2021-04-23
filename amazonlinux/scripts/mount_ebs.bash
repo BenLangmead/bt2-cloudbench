aa31718764a47f461d005323245b74e300c793e0
Ben Langmead <ben.langmead@gmail.com>
2021-04-23 08:59:38 -0400

    add missing inst files

--- amazonlinux/scripts/mount_ebs.bash
+++ amazonlinux/scripts/mount_ebs.bash
@@ -3,9 +3,14 @@
 set -ex
 
 if [ ! -d /work ] ; then
-    dv=$(lsblk --output NAME,SIZE | awk "\$2 == \"${VOLUME_GB}G\"" | cut -f1 -d' ')
-    mkfs -q -t ext4 /dev/${dv}
+    if [[ -z "${VOLUME_NAME}" || "${VOLUME_NAME}" == "None" ]] ; then
+        dv=$(lsblk --output NAME,SIZE | awk "\$2 == \"${VOLUME_GB}G\"" | cut -f1 -d' ')
+    else
+        dv="${VOLUME_NAME}"
+    fi
+    test -e "/dev/${dv}"
+    mkfs -q -t ext4 "/dev/${dv}"
     mkdir /work
-    mount /dev/${dv} /work/
+    mount "/dev/${dv}" /work/
     chmod a+w /work
 fi
