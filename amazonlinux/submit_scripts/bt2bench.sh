dedc226b9b2593b7c117c5cac5e8a8f8ca7697fa
Ben Langmead <ben.langmead@gmail.com>
2021-04-23 09:53:19 -0400

    add more instance types to benchmark

--- amazonlinux/submit_scripts/bt2bench.sh
+++ amazonlinux/submit_scripts/bt2bench.sh
@@ -5,7 +5,10 @@ set -ex
 expt_name=bench-bt2
 profile=jhu_ue2
 
-for i in m5.4xlarge m6g.4xlarge ; do
+for i in m5.4xlarge m6g.4xlarge m6gd.4xlarge c5n.18xlarge \
+         c5.4xlarge c5a.4xlarge c6g.4xlarge \
+         c5d.4xlarge c5ad.4xlarge c6gd.4xlarge
+do
     nm="${expt_name}.${i}"
     rm -rf ".${nm}"
     screen -S ${nm} -dm bash -c \
